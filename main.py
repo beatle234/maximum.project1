e6e80d040ae481f1fd36552770c32b53fbd579d9
dara <a159896@yandex.ru>
2023-07-28 18:25:08 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+#есть строка aabbccdd. нужно посчитать количество каждой из букв
+#множество - список уникальных значений(ффвв - фв)
+
+#def strconter(s):
+#    for symbol in set(s): #отвечает за перебор искомых значений
+#        counter = 0
+#        for sub_symbol in s: #отвечает за подсчет количества искомого символа в строке
+#            if sub_symbol == symbol:
+#                counter += 1
+#        print(symbol, counter)
+
+#strconter('aabcd')
+
+#или
+
+def strconter(s):
+    syms_counter = {}
+    for symbol in s:
+        syms_counter[symbol] = syms_counter.get(symbol, 0) +1
+
+    for symbol, count in syms_counter.items():
+        print(symbol, count)
+
+strconter('hello world')
+
