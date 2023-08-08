0a1d39f50b7260e65f368c4b03a5092c935e81a9
dara <a159896@yandex.ru>
2023-08-08 17:47:57 +0300

    orm

--- advertisements/app_advertisements/models.py
+++ advertisements/app_advertisements/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Advertisement(models.Model):
+     title = models.CharField('заголовок', max_length=128)
+     description = models.TextField('описaние')
+     price = models.DecimalField('цена', max_digits=10, decimal_places=2)
+     auction = models.BooleanField('торг', help_text='Oтметьте, если торг уместен')
+     created_ad = models.DateTimeField(auto_now_add=True)
+     update_ad = models.DateTimeField(auto_now=True)
