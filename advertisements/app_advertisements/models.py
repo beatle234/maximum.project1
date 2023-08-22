cf77afa9448a411bc8692deaab77d17023b7a5e5
dara <a159896@yandex.ru>
2023-08-22 15:19:30 +0300

    hm8

--- advertisements/app_advertisements/models.py
+++ advertisements/app_advertisements/models.py
@@ -21,6 +21,15 @@ class Advertisement(models.Model):
                )
           return self.created_ad.strftime('%d.%m.%Y в %H:%M:%S')
 
+     @admin.display(description='дата последнего обновления')
+     def updated_date(self):
+         from django.utils import timezone
+         if self.update_ad.date() == timezone.now().date():
+             created_time = self.update_ad.time().strftime('%H:%M:%S')
+             return format_html(
+                 '<span style="color: green; font-weight:bold;">Сегодня в {}</span>', created_time
+             )
+         return self.update_ad.strftime('%d.%m.%Y в %H:%M:%S')
 
      def __str__(self):
           return f'Advertisement(id={self.id}, title={self.title}, price={self.price})'
