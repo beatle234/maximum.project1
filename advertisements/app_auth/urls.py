38dfe3b474f32b65e7b015a971932671098a4098
dara <a159896@yandex.ru>
2023-08-29 16:55:26 +0300

    attrs_les11

---
+++ advertisements/app_auth/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import profile_view, login_view, logout
+
+urlpatterns = [
+    path('profile/', profile_view, name = 'profile'),
+    path('login/', login_view, name = 'login'),
+    path('logout/', logout, name = 'logout')
+]
