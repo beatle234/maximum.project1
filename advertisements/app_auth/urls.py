417ba8171dd0c8e419e5da6c245246c4d23622b8
dara <a159896@yandex.ru>
2023-09-01 17:45:59 +0300

    hm11

--- advertisements/app_auth/urls.py
+++ advertisements/app_auth/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import profile_view, login_view, logout
+from .views import profile_view, login_view, logout, register_view
 
 urlpatterns = [
     path('profile/', profile_view, name = 'profile'),
     path('login/', login_view, name = 'login'),
-    path('logout/', logout, name = 'logout')
+    path('logout/', logout, name = 'logout'),
+    path('register/', register_view, name='register')
 ]
