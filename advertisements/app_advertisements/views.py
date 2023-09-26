faecc3a06faf785b01bce83b320d0d8aaa13982d
dara <a159896@yandex.ru>
2023-09-26 17:52:13 +0300

    les12_1:07

--- advertisements/app_advertisements/views.py
+++ advertisements/app_advertisements/views.py
@@ -5,9 +5,13 @@ from .models import Advertisement
 from .forms import AdvertisementForm
 
 def index(request):
-    advertisements = Advertisement.objects.all()
-    context = {'advertisements': advertisements}
-    return render(request, 'app_advertisements/index.html', context)
+    title = request.GET.get("query")
+    if title:
+        advertisments = Advertisement.objects.filter(title=title)
+    else:
+        advertisements = Advertisement.objects.all()
+        context = {'advertisements': advertisements}
+        return render(request, 'app_advertisements/index.html', context)
 
 def top_sellers(request):
     return render(request, 'app_advertisements/top-sellers.html')
